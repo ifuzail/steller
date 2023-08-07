a09574b8fb3368938adae3dd9db9f073e8725706
Fuzail Khan <ifuzail1234@gmail.com>
2023-08-07 21:44:39 +0530

    added cleck auth

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { FaArrowRight, FaShoppingCart, FaFilter, FaHeart } from 'react-icons/fa';
-import CartPage from './cart';
-import useCartStore from '@/store/cartFunc';
-import Link from 'next/link';
-import Sidebar from './Sidebar';
+import React, { useState, useEffect } from "react";
+import {
+  FaArrowRight,
+  FaShoppingCart,
+  FaFilter,
+  FaHeart,
+} from "react-icons/fa";
+import CartPage from "./cart";
+import useCartStore from "@/store/cartFunc";
+import Link from "next/link";
+import Sidebar from "./Sidebar";
+import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   const isCartOpen = useCartStore((state) => state.isCartOpen);
@@ -16,13 +22,10 @@ const Navbar = () => {
     setIsFilterOpen(!isFilterOpen);
   };
 
-  
-
   return (
-    <nav className='bg-gray-50 shadow-xl fixed top-0 left-0 right-0 z-10'>
+    <nav className="bg-gray-50 shadow-xl fixed top-0 left-0 right-0 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 gap-5">
-          
           <Link href="/">
             <div className="flex items-center justify-center gap-2">
               <img src="/logo.png" width={28} height={24} alt="" />
@@ -30,16 +33,23 @@ const Navbar = () => {
             </div>
           </Link>
           <div className="ml-4 relative flex justify-between items-center gap-9">
-          <Link href='/favorites/'>
-          <button className="bg-transparent  text-slate-700 hover:text-slate-900 focus:outline-none mt-1">
-            <FaHeart fontSize={20}/>
-          </button>
-          </Link>
-          <button  className="bg-transparent  text-slate-700 hover:text-slate-900 focus:outline-none"
-              aria-label="filter" onClick={handleFilterToggle}>
-            <FaFilter fontSize={19} />
-          </button>
-          {isFilterOpen && <Sidebar />}
+            <div>
+              <UserButton afterSignOutUrl="/" />
+            </div>
+            <Link href="/favorites/">
+              <button className="bg-transparent  text-slate-700 hover:text-slate-900 focus:outline-none mt-1">
+                <FaHeart fontSize={20} />
+              </button>
+            </Link>
+
+            <button
+              className="bg-transparent  text-slate-700 hover:text-slate-900 focus:outline-none"
+              aria-label="filter"
+              onClick={handleFilterToggle}
+            >
+              <FaFilter fontSize={19} />
+            </button>
+            {isFilterOpen && <Sidebar />}
             <button
               className="bg-transparent text-slate-700 hover:text-slate-900 focus:outline-none"
               aria-label="Cart"
@@ -54,8 +64,8 @@ const Navbar = () => {
                 <span
                   className={
                     isCartOpen
-                      ? 'hidden'
-                      : 'absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center'
+                      ? "hidden"
+                      : "absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center"
                   }
                 >
                   {cartItems.length}
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import '@/styles/globals.css'
+import { ClerkProvider } from '@clerk/nextjs';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+     <ClerkProvider {...pageProps}>
       <ToastContainer  className="absolute top-0 right-0 mt-8 mr-8 z-50"
         toastClassName="rounded-md shadow-md"
         bodyClassName="p-5 text-black"
         autoClose={2000}/>
       <Component {...pageProps} />
+      </ClerkProvider>
     </>
   ); 
 }
